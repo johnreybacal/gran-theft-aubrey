8bb00e2fa6bf5ef2a56a2b8cfdc031ad057398e6
Johnrey Bacal <johnrey.bacal@yahoo.com>
2026-03-20 09:41:11 +0800

    fix end

--- scripts/end.gd
+++ scripts/end.gd
@@ -7,7 +7,6 @@ extends Node2D
 func _ready() -> void:
     hide()
     camera.enabled = false
-    _show_end()
     EventBus.on_encounter_end.connect(_show_end.call_deferred.unbind(2))
     EventBus.on_escape.connect(_show_end.call_deferred)
 
@@ -17,14 +16,16 @@ func _show_end():
         show()
         camera.enabled = true
         if StateManager.is_busted:
-            label.text = "GRANY OTTO FINALLY CAUGHT!\n\nPress A to restart"
+            label.text = "GRANY OTTO FINALLY CAUGHT!"
             $SFX/Lose.play()
             $AnimatedSprite2D.play("lose")
         else:
-            label.text = "GRANNY OTTO STRIKES AGAIN!\n\nPress A to restart"
+            label.text = "GRANNY OTTO STRIKES AGAIN!"
             $SFX/Win.play()
             $AnimatedSprite2D.play("win")
 
 
 func _on_restart_button_pressed() -> void:
+    EventBus.on_encounter_end.disconnect(_show_end.call_deferred.unbind(2))
+    EventBus.on_escape.disconnect(_show_end.call_deferred)
     get_tree().change_scene_to_file("res://scenes/start.tscn")
