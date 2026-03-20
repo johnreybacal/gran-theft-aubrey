3fde2377d1a6b80b21259019bd84b52710d15b96
Johnrey Bacal <johnrey.bacal@yahoo.com>
2026-03-20 09:07:01 +0800

    end animation

--- scripts/end.gd
+++ scripts/end.gd
@@ -7,6 +7,7 @@ extends Node2D
 func _ready() -> void:
     hide()
     camera.enabled = false
+    _show_end()
     EventBus.on_encounter_end.connect(_show_end.call_deferred.unbind(2))
     EventBus.on_escape.connect(_show_end.call_deferred)
 
@@ -16,8 +17,10 @@ func _show_end():
         show()
         camera.enabled = true
         if StateManager.is_busted:
-            label.text = "BUSTED"
+            label.text = "GRANY OTTO FINALLY CAUGHT!\n\nPress A to restart"
             $SFX/Lose.play()
+            $AnimatedSprite2D.play("lose")
         else:
-            label.text = "GRANNY OTTO STRIKES AGAIN!"
+            label.text = "GRANNY OTTO STRIKES AGAIN!\n\nPress A to restart"
             $SFX/Win.play()
+            $AnimatedSprite2D.play("win")
